a929e5c1ef303f3b327926f7c8f1de0d7a9fefd3
Hagino Takahiro <hagino@apache.org>
2019-01-13 18:00:18 +0900

    Adjusting setup information

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ def read(filename):
 
 
 setup(
-    name="auto-ml",
+    name="autoprep",
     version="0.0.1",
-    author="Takahiro Hagino",
+    author="Hagino Takahiro",
     author_email="haginota@gmail.com",
-    description=("Automated Data Preprocessing for Machine Learning.",
+    description=("Automated Data Pre-processing for Machine Learning.",
                  "Optimized for Pandas DataFrame."),
     license="Apache Software License",
     keywords="automl preprocessing",
     url="https://github.com/haginot/auto-prep",
-    packages=['autoprep', 'tests'],
+    packages=['autoprep'],
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     classifiers=[]
 )
